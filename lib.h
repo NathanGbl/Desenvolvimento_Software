8cdd118bbe737ab49c2c046410a2e18a49e58a57
NathanGbl <nathangabriel.f.leite@gmail.com>
2023-10-28 15:24:28 -0300

    Função extrato pronta e comentada

--- lib.h
+++ lib.h
@@ -1,18 +1,39 @@
 #include <stdio.h>
 
 typedef struct {
+  char operacao[13];
+  float valor;
+} dados_extrato; // Criação do struct com os dados para cada movimentação de dinheiro dentro da conta.
+
+typedef struct {
   char nome[101];
   char cpf[50];
   char tipo_conta[50];
   float saldo;
   char senha[101];
-} cliente;
+  dados_extrato extrato[100];
+  int qtnd;
+} cliente; // Criação do struct responsável por armazenar as informações do cliente.
 
 typedef struct {
-  cliente cliente[10];
+  cliente cliente[1000];
   int qtnd;
-} lista_clientes;
+} lista_clientes; // Criação do struct com a lista de todos os structs de cada cliente.
 
-void menu(int *opcao);
+void menu(int *opcao, int *cont);
 
 void novo_cliente(lista_clientes *lc);
+
+void extrato(lista_clientes *lc);
+
+void cria_arquivo(char *nome, FILE *f, char cpf[12]);
+
+void transf(lista_clientes *lc);
+
+void salva_bin(lista_clientes *lc);
+
+void le_bin(lista_clientes *lc);
+
+void cria_bin();
+
+int menu2();
